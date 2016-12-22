eab80149679f0c0ac547bb5495a6ca7f3c0f3bf5
Maxim Galushka <mgalushka@gmail.com>
2016-12-22 20:06:11 +0000

    Fixes to make it compile

--- src/utils.c
+++ src/utils.c
@@ -57,7 +57,7 @@ void print_error(char *str, ...)
 void print_debug(char *str, ...)
 {
     if (!debug) return;
-    
+
     va_list ap;
     char buf[200];
     va_start(ap, str);
@@ -72,7 +72,7 @@ uint32_t sum_every_16bits(void *addr, int count)
 {
     register uint32_t sum = 0;
     uint16_t * ptr = addr;
-    
+
     while( count > 1 )  {
         /*  This is the inner loop */
         sum += * ptr++;
@@ -95,7 +95,7 @@ uint16_t checksum(void *addr, int count, int start_sum)
     uint32_t sum = start_sum;
 
     sum += sum_every_16bits(addr, count);
-    
+
     /*  Fold 32-bit sum to 16 bits */
     while (sum>>16)
         sum = (sum & 0xffff) + (sum >> 16);
@@ -105,8 +105,8 @@ uint16_t checksum(void *addr, int count, int start_sum)
 
 int get_address(char *host, char *port, struct sockaddr *addr)
 {
-    struct addrinfo hints;
-    struct addrinfo *result, *rp;
+    addrinfo hints;
+    addrinfo *result, *rp;
     int s;
 
     memset(&hints, 0, sizeof(struct addrinfo));
@@ -125,7 +125,7 @@ int get_address(char *host, char *port, struct sockaddr *addr)
         freeaddrinfo(result);
         return 0;
     }
-    
+
     return 1;
 }
 
